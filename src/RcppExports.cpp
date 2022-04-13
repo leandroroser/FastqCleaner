b48cb38874379a824469ad48edbcfe70035f1233
leandroroser <learoser@gmail.com>
2022-04-12 23:49:52 -0300

    push upstrean

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // cpp_GC_content
 NumericMatrix cpp_GC_content(StringMatrix x);
 RcppExport SEXP _FastqCleaner_cpp_GC_content(SEXP xSEXP) {
